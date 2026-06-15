03676a905a65af38840c882fcdcdc956e42dfc0e
Akksheetha <akkshee@gmail.com>
2026-06-15 21:51:54 +0530

    Typicode practice code commit

---
+++ src/test/java/typicode_practice/Put.java
@@ -0,0 +1,34 @@
+package typicode_practice;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Put {
+
+	@Test
+	public void post_routes() {
+		String requestBody =
+				"{"
+						+ "\"title\":\"Akkshee Updated\","
+						+ "\"body\":\"Learning API\","
+						+ "\"userId\":1"
+						+ "}";
+
+    Response res = RestAssured
+            .given()
+            .header("Content-Type", "application/json")
+            .body(requestBody)
+            .when()
+            .put("https://jsonplaceholder.typicode.com/posts/1");
+
+    System.out.println("Status Code : " + res.getStatusCode());
+
+    res.prettyPrint();
+
+    // Validation 1
+    Assert.assertEquals(res.getStatusCode(), 200);
+	}
+}
